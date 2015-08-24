cea3ca477c7545df93be831c89180f431d3df918
Josue Valverde & Kendra Ash <student@epicodus.com>
2015-08-24 10:12:46 -0700

    add initial files and make sure specs run with ActiveRecord.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+ENV['RACK_ENV'] = 'test'
+
+require('rspec')
+require('pg')
+require('sinatra/activerecord')
+require('list')
+require('task')
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each() do |task|
+      task.destroy()
+    end
+  end
+end
---
+++ spec/task_spec.rb
@@ -0,0 +1,17 @@
+require('spec_helper')
+
+describe(Task) do
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.create({:description => "learn SQL", :done => false, :rank => 2})
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+end
